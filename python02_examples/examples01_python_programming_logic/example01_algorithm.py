61a110136000b56f3bbae9cc327e4bd8697b47ae
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-11-24 08:20:32 -0500

    developmet project python

---
+++ python02_examples/examples01_python_programming_logic/example01_algorithm.py
@@ -0,0 +1,30 @@
+# Descripción del Objetivo o Problema a resolver, organizado como Sistema [Entradas, Proceso y Salidas]
+	
+# Declarar variables, constantes y/o arreglos.
+	
+# Dimensionar arreglos.
+	
+# Iniciar variables, constantes y/o arreglos
+	
+# Entrada de Datos: Pueden ser solicitados o no
+	
+# Proceso: Determinar los requerimientos y validaciones. Generalmente,	
+#          se usan las estructuras de control: Secuencial, condicional, repetición.
+	
+# Salida de Datos. Resultado(s).
+
+# Comentarios
+'''
+Varias líneas
+de código
+comentadas con 
+comilla sencilla
+'''
+
+"""
+varias líneas
+de código 
+comentadas con
+comilla doble
+tres veces
+"""
