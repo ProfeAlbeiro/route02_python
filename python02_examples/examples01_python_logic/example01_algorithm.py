d3fcf21e559b4cdadf25368d463322445b2a9b10
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-01-16 10:18:47 -0500

    development project Python

--- python02_examples/examples01_python_logic/example01_algorithm.py
+++ python02_examples/examples01_python_logic/example01_algorithm.py
@@ -1,3 +1,18 @@
+# Descripción del Objetivo o Problema a resolver, organizado como Sistema [Entradas, Proceso y Salidas]
+	
+# Declarar constantes, variables, y/o arreglos.
+	
+# Dimensionar arreglos.
+	
+# Iniciar constantes, variables, y/o arreglos
+	
+# Entrada de Datos: Pueden ser solicitados o no
+	
+# Proceso: Determinar los requerimientos y validaciones. Generalmente,	
+#          se usan las estructuras de control: Secuencial, condicional, repetición.
+	
+# Salida de Datos: Resultado(s). Pueden ser parte del proceso.
+
 '''
 Varias líneas
 de código
@@ -12,18 +27,3 @@ comentadas con
 comilla doble
 tres veces
 """
-
-# Descripción del Objetivo o Problema a resolver, organizado como Sistema [Entradas, Proceso y Salidas]
-	
-# Declarar constantes, variables, y/o arreglos.
-	
-# Dimensionar arreglos.
-	
-# Iniciar constantes, variables, y/o arreglos
-	
-# Entrada de Datos: Pueden ser solicitados o no
-	
-# Proceso: Determinar los requerimientos y validaciones. Generalmente,	
-#          se usan las estructuras de control: Secuencial, condicional, repetición.
-	
-# Salida de Datos. Resultado(s).
