50e6f8a63f0d1b5f6b9ffa136cdc15fed8b70716
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-01-25 12:50:08 -0500

    development project Python - Django

--- python04_projects/project05_django/dj02_mysite/dj02_mysite/asgi.py
+++ python04_projects/project05_django/dj02_mysite/dj02_mysite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dj02_site project.
+ASGI config for dj02_mysite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj02_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj02_mysite.settings')
 
 application = get_asgi_application()
