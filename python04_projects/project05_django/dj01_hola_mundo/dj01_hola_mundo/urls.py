9750760d1a8437c961ce5ba934bb4f7ca7e53176
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-01-25 12:03:49 -0500

    development project Python - Django

--- python04_projects/project05_django/dj01_hola_mundo/dj01_hola_mundo/urls.py
+++ python04_projects/project05_django/dj01_hola_mundo/dj01_hola_mundo/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', views.saludo, name="saludo"),
+    path('despedida/', views.despedida, name="despedida"),
 ]
