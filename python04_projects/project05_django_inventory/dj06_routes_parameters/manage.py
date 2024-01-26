db567b0b0a711adce79186e7fb578defed45727b
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-01-26 07:38:38 -0500

    development project Python - Django

--- python04_projects/project05_django_inventory/dj06_routes_parameters/manage.py
+++ python04_projects/project05_django_inventory/dj06_routes_parameters/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj06_routes_with_parameters.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj06_routes_parameters.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
