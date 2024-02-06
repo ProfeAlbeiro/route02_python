0597086c4927118cce1fbe326c66f1b48f276d34
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-02-06 12:27:58 -0500

    development project Python - Django

--- python04_projects/project05_django_inventory/dj11_static_files/dj11_static_files/settings.py
+++ python04_projects/project05_django_inventory/dj11_static_files/dj11_static_files/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+    # '/var/www/assets',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
