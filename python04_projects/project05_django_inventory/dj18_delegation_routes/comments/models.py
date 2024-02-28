3bf0adfd5da97e27cd606034c66d42e4c5f8e5ed
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-02-28 16:52:33 -0500

    development project Python - Django

--- python04_projects/project05_django_inventory/dj18_delegation_routes/comments/models.py
+++ python04_projects/project05_django_inventory/dj18_delegation_routes/comments/models.py
@@ -4,7 +4,7 @@ class Comment(models.Model):
 
     name = models.CharField(max_length=255, null=False)
     score = models.IntegerField(default=3)
-    commet = models.TextField(max_length=1000, null=True)
+    comment = models.TextField(max_length=1000, null=True)
     date = models.DateField(null=True)
     signature = models.CharField(max_length=100, default="Firma")
 
--- python04_projects/project05_django_inventory/dj19_create_delete_data/comments/views.py
+++ python04_projects/project05_django_inventory/dj19_create_delete_data/comments/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Comment
 
-# Create your views here.
+def test(request):
+    return HttpResponse("Funciona correctamente")
+
+def create(request):
+    comment = Comment(name="Juanjo", score=5, comment="Este es un comentario")
+    comment.save()
+    return HttpResponse("Ruta para la creación de modelos")
