57758b900829ecce67ed1923536737de2ef8e4ff
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-02-29 12:33:23 -0500

    development project Python - Django

---
+++ python04_projects/project05_django_inventory/dj22_data_query_partone/dj22_data_query_partone/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dj22_data_query_partone project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj22_data_query_partone.settings')
+
+application = get_asgi_application()
---
+++ python04_projects/project05_django_inventory/dj22_data_query_partone/post/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Author, Entry
+
+def queries(request):
+    # Obtener todos los elementos
+    authors = Author.objects.all()
+
+    # Obtener datos filtrados por condición
+    filtered = Author.objects.filter(email='laurenhicks@example.net')
+
+    # Obtener un único objeto filtrado
+    author = Author.objects.get(id=1)
+    
+    # Obtener los 10 primeros elementos
+    limits = Author.objects.all()[:10]
+    
+    # Obtener 5 elementos saltando los 5 primeros
+    offsets = Author.objects.all()[5:10]
+
+    return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets})
