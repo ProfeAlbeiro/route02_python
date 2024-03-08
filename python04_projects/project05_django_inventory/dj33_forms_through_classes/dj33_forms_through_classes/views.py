1e07bc05e810d26fc32deac32a674d8d11c422ee
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-03-08 11:27:25 -0500

    development project Python - Django

---
+++ python04_projects/project05_django_inventory/dj33_forms_through_classes/dj33_forms_through_classes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import CommentForm
+
+def form(request):
+    comment_form = CommentForm({'name': 'Juanjo', 'url': 'https://open-bootcamp.com', 'comment': 'Comentario'})
+    return render(request, 'form.html', {'comment_form': comment_form})
+
+def goal(request):
+    if request.method != "POST":
+        return HttpResponse("Método no permitido")
+    
+    return HttpResponse(request.POST['name'])
+    
+
