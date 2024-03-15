d646dfd5e8210414de1cf6362dce7d80b91d321b
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-03-15 16:26:37 -0500

    development project Python - Django

--- python04_projects/project05_django_inventory/dj42_practical_staticforms_layouts/dj42_practical_staticforms_layouts/settings.py
+++ python04_projects/project05_django_inventory/dj42_practical_staticforms_layouts/dj42_practical_staticforms_layouts/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dj42_practical_staticforms_layouts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "agenda",
+        "USER": "postgres",
+        "PASSWORD": "AlbeiroRamos123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
