8d09616a372c3489065cfca9a498fd1d1676f4da
ProfeAlbeiro <profealbeiro2020@gmail.com>
2024-03-20 17:31:42 -0500

    FrameWork Django

--- python02_examples/examples06_python_frameworks/frameworks01_django/dj44_practical_crud_contacts/contact/urls.py
+++ python02_examples/examples06_python_frameworks/frameworks01_django/dj44_practical_crud_contacts/contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [    
-    path('', views.index, name="contact")
+    path('', views.index, name="contact"),
+    path('view/<int:id>', views.view, name="contact_view"),
 ]
